43940a88964b0e9a7fcc6b871e42c350b8cfe3d8
RozeFound <rozefound@gmail.com>
2022-07-22 05:05:19 +0700

    Whoopsie

--- handlers.py
+++ handlers.py
@@ -12,7 +12,7 @@ async def help(message: types.Message) -> None:
     reply += "Доступные команды:\n"
     reply += "/about или /help - Выведет это сообщение\n"
     reply += "/ping - Выведет сообщение о текущей системе\n"
-    reply += "/topNwords - Выведет топ N слов в чате (Информация с GRStats)"
+    reply += "/topNwords - Выведет топ N слов в чате (Информация с GRStats)\n"
     reply += "/random_anime - Выдаёт случайное аниме с ShikiMori"
 
     await message.answer(reply)
